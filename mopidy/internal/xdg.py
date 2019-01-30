7168226969b9ae5b4806009d968f743c6dae0954
Josue Ortega <josue@debian.org>
2019-01-29 22:37:07 -0600

    Fix user dirs config loading

--- mopidy/internal/xdg.py
+++ mopidy/internal/xdg.py
@@ -60,9 +60,9 @@ def _get_user_dirs(xdg_config_dir):
     data = data.replace(b'"', b'')
 
     config = configparser.RawConfigParser()
-    config.read_file(io.BytesIO(data).getvalue().decode('utf-8'))
+    config.read_string(io.BytesIO(data).read().decode('utf-8'))
 
     return {
-        k.upper().decode('utf-8'): os.path.abspath(v)
+        k.upper(): os.path.abspath(v)
         for k, v in config.items('XDG_USER_DIRS') if v is not None
     }
