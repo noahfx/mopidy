11ceafbf7661b406d88e42e15f2519bde29677ab
Josue Ortega <josue@debian.org>
2019-01-29 22:03:41 -0600

    Remove python 2.7 verification at init

--- mopidy/__init__.py
+++ mopidy/__init__.py
@@ -5,11 +5,6 @@ import sys
 import warnings
 
 
-if not (2, 7) <= sys.version_info < (3,):
-    sys.exit(
-        'ERROR: Mopidy requires Python 2.7, but found %s.' %
-        platform.python_version())
-
 
 warnings.filterwarnings('ignore', 'could not open display')
 
